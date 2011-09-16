9cab07133dda4b783deb174fd531d116e6276546
Nick Pohodnya <nick.pohodnya@gmail.com>
2011-09-16 15:29:42 +0300

    better load

--- lib/enum_column.rb
+++ lib/enum_column.rb
@@ -1,16 +1,18 @@
 if defined?(::Rails::Railtie)
   class EnumColumnRailtie < Rails::Railtie
-    ActiveSupport.on_load :active_record do
-      require 'enum/mysql_adapter'
-      require 'enum/enum_adapter'
-      require 'enum/schema_statements'
-      require 'enum/schema_definitions'
-      require 'enum/quoting'
-      require 'enum/validations'
-    end
+    initializer 'enum_column.initialize', :after => 'active_record.initialize_database' do |app|
+      ActiveSupport.on_load :active_record do
+        require 'enum/mysql_adapter'
+        require 'enum/enum_adapter'
+        require 'enum/schema_statements'
+        require 'enum/schema_definitions'
+        require 'enum/quoting'
+        require 'enum/validations'
+      end
 
-    ActiveSupport.on_load :action_view do
-      require 'enum/active_record_helper'
+      ActiveSupport.on_load :action_view do
+        require 'enum/active_record_helper'
+      end
     end
   end
 end
---
+++ lib/enum_column3.rb
@@ -0,0 +1,1 @@
+require 'enum_column'
