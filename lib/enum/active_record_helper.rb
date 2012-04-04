51ba45fe45bc16d91a653c317048a35bc052bb3e
Dan McGowan <dantmcgowan@mac.com>
2012-04-04 11:05:08 -0700

    added methods for radio buttons

--- lib/enum/active_record_helper.rb
+++ lib/enum/active_record_helper.rb
@@ -6,6 +6,7 @@ if defined?(Rails::Generator)
           return (@field_type = :enum_select) if type == :enum
           field_type_without_enumerated_attribute
         end
+
         alias_method_chain :field_type, :enumerated_attribute
       end
     end
@@ -13,51 +14,85 @@ if defined?(Rails::Generator)
 end
 
 if defined?(ActionView::Base)
-	module ActionView
-		module Helpers
+  module ActionView
+    module Helpers
 
-			#form_options_helper.rb
-			module FormOptionsHelper
-				#def select
-				def enum_select(object, method, options={}, html_options={})
-					InstanceTag.new(object, method, self, options.delete(:object)).to_enum_select_tag(options, html_options)
-				end
-			end
+      #form_options_helper.rb
+      module FormOptionsHelper
+        #def select
+        def enum_select(object, method, options={}, html_options={})
+          InstanceTag.new(object, method, self, options.delete(:object)).to_enum_select_tag(options, html_options)
+        end
 
-			class InstanceTag
-				def to_enum_select_tag(options, html_options={})
-					if self.object.respond_to?(method_name.to_sym)
+        def enum_radio(object, method, options={}, html_options={})
+          InstanceTag.new(object, method, self, options.delete(:object)).to_enum_radio_tag(options, html_options)
+        end
+      end
+
+      class InstanceTag
+        def to_enum_select_tag(options, html_options={})
+          if self.object.respond_to?(method_name.to_sym)
             column = self.object.column_for_attribute(method_name)
-						if (value = self.object.__send__(method_name.to_sym))
-							options[:selected] ||= value.to_s
-						else
+            if (value = self.object.__send__(method_name.to_sym))
+              options[:selected] ||= value.to_s
+            else
               options[:include_blank] = column.null if options[:include_blank].nil?
-						end
-					end
-					to_select_tag(column.limit, options, html_options)
-				end
+            end
+          end
+          to_select_tag(column.limit, options, html_options)
+        end
 
-				#initialize record_name, method, self
-				if respond_to?(:to_tag)
-  				def to_tag_with_enumerated_attribute(options={})
-  					#look for an enum
-  					if (column_type == :enum && self.object.class.respond_to?(method_name.to_sym))
-  						to_enum_select_tag(options)
-  					else
-  						to_tag_without_enumerated_attribute(options)
-  					end
-  				end
-  				alias_method_chain :to_tag, :enumerated_attribute
-				end
+        def to_enum_radio_tag(options, html_options={})
+          if self.object.respond_to?(method_name.to_sym)
+            column = self.object.column_for_attribute(method_name)
+            if (value = self.object.__send__(method_name.to_sym))
+              options[:selected] ||= value.to_s
+            else
+              options[:include_blank] = column.null if options[:include_blank].nil?
+            end
+          end
+          values = column.limit
+          raise ArgumentError, "No values for enum select tag" unless values
+          tag_text = ''
+          template = options.dup
+          template.delete(:selected)
+          values.each do |enum|
+            opts = template.dup
+            opts['checked'] = 'checked' if options[:selected] and options[:selected] == enum
+            opts['id'] = "#{opts['id']}_#{enum}"
+            tag_text << to_radio_button_tag(enum, opts)
+            tag_text << "<label>#{enum} "
+            tag_text << "</label>"
+          end
+          tag_text.html_safe
+        end
 
-			end
+        #initialize record_name, method, self
+        if respond_to?(:to_tag)
+          def to_tag_with_enumerated_attribute(options={})
+            #look for an enum
+            if (column_type == :enum && self.object.class.respond_to?(method_name.to_sym))
+              to_enum_select_tag(options)
+            else
+              to_tag_without_enumerated_attribute(options)
+            end
+          end
 
-			class FormBuilder
-				def enum_select(method, options={}, html_options={})
-					@template.enum_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
-				end
-			end
+          alias_method_chain :to_tag, :enumerated_attribute
+        end
 
-		end
-	end
+      end
+
+      class FormBuilder
+        def enum_select(method, options={}, html_options={})
+          @template.enum_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
+        end
+
+        def enum_radio(method, options={}, html_options={})
+          @template.enum_radio(@object_name, method, objectify_options(options), @default_options.merge(html_options))
+        end
+      end
+
+    end
+  end
 end
