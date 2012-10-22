6a7b9e134d0fe92629a2ea4c57f7b5bf18b567b9
Isaac Sloan <isaac@isaacsloan.com>
2012-10-22 17:27:10 -0600

    formatting

--- lib/enum/active_record_helper.rb
+++ lib/enum/active_record_helper.rb
@@ -13,37 +13,37 @@ if defined?(Rails::Generator)
 end
 
 if defined?(ActionView::Base)
-	module ActionView
-		module Helpers
+  module ActionView
+    module Helpers
 
-	#form_options_helper.rb
-	module FormOptionsHelper
-		#def select
-		def enum_select(object, method, options={}, html_options={})
-			InstanceTag.new(object, method, self, options.delete(:object)).to_enum_select_tag(options, html_options)
-		end
-	end
-	
-	class InstanceTag
-		def to_enum_select_tag(options, html_options={})
-			if self.object.respond_to?(method_name.to_sym)
-			  column = self.object.column_for_attribute(method_name)
-			  if (value = self.object.__send__(method_name.to_sym))
-			    options[:selected] ||= value.to_s
-			  else
-			    options[:include_blank] = column.null if options[:include_blank].nil?
-			  end
-			end
-			to_select_tag(column.limit.select{|o| !o.blank?}.map{|v|[v.to_s.humanize,v]}, options, html_options)
-		end
-	end
-	
-	class FormBuilder
-		def enum_select(method, options={}, html_options={})
-			@template.enum_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
-		end
-	end
+      #form_options_helper.rb
+      module FormOptionsHelper
+        #def select
+        def enum_select(object, method, options={}, html_options={})
+          InstanceTag.new(object, method, self, options.delete(:object)).to_enum_select_tag(options, html_options)
+        end
+      end
 
-		end
-	end
+      class InstanceTag
+        def to_enum_select_tag(options, html_options={})
+          if self.object.respond_to?(method_name.to_sym)
+            column = self.object.column_for_attribute(method_name)
+            if (value = self.object.__send__(method_name.to_sym))
+              options[:selected] ||= value.to_s
+            else
+              options[:include_blank] = column.null if options[:include_blank].nil?
+            end
+          end
+          to_select_tag(column.limit.select{|o| !o.blank?}.map{|v|[v.to_s.humanize,v]}, options, html_options)
+        end
+      end
+
+      class FormBuilder
+        def enum_select(method, options={}, html_options={})
+          @template.enum_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
+        end
+      end
+
+    end
+  end
 end
