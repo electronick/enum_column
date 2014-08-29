28a944ab08ccc7151c763cce50773100e9adcec6
bmck <bill.mckinnon@bmck.org>
2014-08-29 13:46:35 -0500

    Add new file enum/abstract_mysql_adapter.rb

--- lib/enum_column.rb
+++ lib/enum_column.rb
@@ -3,6 +3,7 @@ if defined?(::Rails::Railtie)
     initializer 'enum_column.initialize', :after => 'active_record.initialize_database' do |app|
       ActiveSupport.on_load :active_record do
         require 'enum/mysql_adapter'
+        require 'enum/abstract_mysql_adapter'
         require 'enum/enum_adapter'
         require 'enum/schema_statements'
         require 'enum/schema_definitions'
