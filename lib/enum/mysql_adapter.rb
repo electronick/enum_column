8ffa96ea4a644185799af53e5b8be5de4b218b5e
Adam Godel <agodel@mdsol.com>
2015-07-01 18:11:28 -0400

    makes compatible with Mysql2SpatialAdapter

--- lib/enum/mysql_adapter.rb
+++ lib/enum/mysql_adapter.rb
@@ -1,5 +1,7 @@
 adapter_class = if defined? ActiveRecord::ConnectionAdapters::MySQLJdbcConnection
   ActiveRecord::ConnectionAdapters::MySQLJdbcConnection
+elsif defined? ActiveRecord::ConnectionAdapters::Mysql2SpatialAdapter
+  ActiveRecord::ConnectionAdapters::Mysql2SpatialAdapter
 elsif defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
   ActiveRecord::ConnectionAdapters::Mysql2Adapter
 elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
@@ -7,7 +9,7 @@ elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
 end
 
 adapter_class.module_eval do
-  alias __native_database_types_enum native_database_types
+  alias __native_database_types_enum native_database_types if defined? native_database_types
 
   def native_database_types #:nodoc
     types = __native_database_types_enum
