01be1420e2e2f204f03af62baa7b57f1046f2383
Adam Godel <agodel@mdsol.com>
2015-07-02 09:32:24 -0400

    makes gem compatible with mysql2spatial adapter

--- lib/enum/mysql_adapter.rb
+++ lib/enum/mysql_adapter.rb
@@ -1,7 +1,7 @@
 adapter_class = if defined? ActiveRecord::ConnectionAdapters::MySQLJdbcConnection
   ActiveRecord::ConnectionAdapters::MySQLJdbcConnection
 elsif defined? ActiveRecord::ConnectionAdapters::Mysql2SpatialAdapter
-  ActiveRecord::ConnectionAdapters::Mysql2SpatialAdapter
+  ActiveRecord::ConnectionAdapters::Mysql2SpatialAdapter::MainAdapter
 elsif defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
   ActiveRecord::ConnectionAdapters::Mysql2Adapter
 elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
@@ -9,11 +9,12 @@ elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
 end
 
 adapter_class.module_eval do
-  alias __native_database_types_enum native_database_types if defined? native_database_types
+  alias __native_database_types_enum native_database_types #if defined? native_database_types
 
   def native_database_types #:nodoc
     types = __native_database_types_enum
     types[:enum] = { :name => "enum" }
     types
   end
+
 end
