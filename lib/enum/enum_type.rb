8dd883ac61de292a9ec578da026165169333d5b1
Schuyler Jager <schuyler@jewlr.com>
2015-08-20 11:01:29 -0400

    Backwards compatability for Rails 4.1

--- lib/enum/enum_type.rb
+++ lib/enum/enum_type.rb
@@ -1,23 +1,25 @@
-module ActiveRecord
-  module Type
-    class Enum < Value # :nodoc:
-      def type
-        :enum
-      end
-
-      def type_cast_for_database
-        value.to_s
-      end
-
-      private
-
-        def cast_value(value)
-          if value == ''
-            nil
-          else
-            value.to_sym
-          end
+if defined? ActiveRecord::Type::Value
+  module ActiveRecord
+    module Type
+      class Enum < Value # :nodoc:
+        def type
+          :enum
         end
+
+        def type_cast_for_database
+          value.to_s
+        end
+
+        private
+
+          def cast_value(value)
+            if value == ''
+              nil
+            else
+              value.to_sym
+            end
+          end
+      end
     end
   end
 end
